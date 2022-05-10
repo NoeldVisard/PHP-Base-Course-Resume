ec7bbe2123a7539ae7843146a3d2a113d8aecda4
Rodion.Karimov <Rodion.Karimov@tatar.ru>
2022-05-11 01:08:43 +0300

    Update ConfigParser.php to parsing json in .env file

--- core/ConfigParser.php
+++ core/ConfigParser.php
@@ -17,8 +17,13 @@ class ConfigParser
     {
         $configFile = file($this->configFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($configFile as $line) {
-            if ($line !== '#') {
-                [$key, $value] = explode('=', $line);
+            if ($line[0] !== '#' && $line[0] !== '{' && $line[0] !== '}') {
+                [$key, $value] = explode(':', $line);
+                $value = substr($value, 0, -1);
+                if ($value[0] === "\"" && $value[strlen($value) - 1] === "\"") {
+                    $value = substr($value, 1);
+                    $value = substr($value, 0, -1);
+                }
                 $_SERVER[$key] = $value;
                 $_ENV[$key] = $value;
                 putenv("{$key}={$value}");
